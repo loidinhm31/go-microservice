19eba5e762829cb8b8148e586313bed8b4730e53
loidinhm31 <loidinh.git@gmail.com>
2022-11-21 00:56:05 +0700

    init microservices

---
+++ front-end/cmd/web/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "test.page.gohtml")
+	})
+
+	fmt.Println("Starting front end service on port 8079")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func render(w http.ResponseWriter, t string) {
+	wDir, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+
+	if !strings.HasSuffix(wDir, "/cmd/web") {
+		err = os.Chdir("./cmd/web/")
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	partials := []string{
+		"./templates/base.layout.gohtml",
+		"./templates/header.partial.gohtml",
+		"./templates/footer.partial.gohtml",
+	}
+
+	var templateSlice []string
+	templateSlice = append(templateSlice, fmt.Sprintf("./templates/%s", t))
+
+	for _, x := range partials {
+		templateSlice = append(templateSlice, x)
+	}
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
